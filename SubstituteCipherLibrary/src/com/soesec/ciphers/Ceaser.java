023d2f7528e1fd32179bb688398b6de9df65206d
soesec13 <sebi.wurst@gmail.com>
2016-08-17 16:55:57 +0200

    Implemented Ceaser Cipher

--- SubstituteCipherLibrary/src/com/soesec/ciphers/Ceaser.java
+++ SubstituteCipherLibrary/src/com/soesec/ciphers/Ceaser.java
@@ -6,7 +6,49 @@
 
 package com.soesec.ciphers;
 
+import com.soesec.factory.ICipher;
+import com.soesec.factory.InfoProvider;
+import com.soesec.util.Alphabet;
 
-public class Ceaser {
 
+public class Ceaser implements ICipher{
+    private InfoProvider info;
+    @Override
+    public String encode(String plain) {
+        String encrypted = "";
+        for(char letter : plain.toUpperCase().toCharArray())
+        {
+            int index = Alphabet.IndexOf(letter);
+            if(index<0)
+            {
+                encrypted+=letter;
+                continue;
+            }
+            int i = (index+info.getRightShift()) % 26;
+            encrypted += Alphabet.At(i);
+        }
+        return encrypted;
+    }
+
+    @Override
+    public String decode(String plain) {
+        String decrypted = "";
+        for(char letter : plain.toUpperCase().toCharArray())
+        {
+            int index = Alphabet.IndexOf(letter);
+            if(index<0)
+            {
+                decrypted+=letter;
+                continue;
+            }
+            int i = (index-info.getRightShift()) % 26;
+            decrypted += Alphabet.At(i);
+        }
+        return decrypted;
+    }
+    @Override
+    public ICipher Init(InfoProvider info) {
+        this.info = info;
+        return this;
+    }
 }
