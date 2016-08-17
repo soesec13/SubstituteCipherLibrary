941aee811498f9361774d4931bc72d219002b897
soesec13 <sebi.wurst@gmail.com>
2016-08-17 16:55:37 +0200

    Implemented Atbash Cipher

--- SubstituteCipherLibrary/src/com/soesec/ciphers/Atbash.java
+++ SubstituteCipherLibrary/src/com/soesec/ciphers/Atbash.java
@@ -6,7 +6,38 @@
 
 package com.soesec.ciphers;
 
+import com.soesec.factory.ICipher;
+import com.soesec.factory.InfoProvider;
+import com.soesec.util.Alphabet;
 
-public class Atbash {
+
+public class Atbash implements ICipher{
+    
+    @Override
+    public String encode(String plain) {
+        String encrypted = "";
+        for(char letter : plain.toUpperCase().toCharArray())
+        {
+            int index =Alphabet.IndexOf(letter);
+            if(index<0)
+            {
+                encrypted+=letter;
+                continue;
+            }
+            int i = (26-(index+1))%26;
+            encrypted += Alphabet.At(i);
+        }
+        return encrypted;
+    }
+
+    @Override
+    public String decode(String plain) {
+        return encode(plain);
+    }
+    
+    @Override
+    public ICipher Init(InfoProvider info) {
+        return this;
+    }
 
 }
