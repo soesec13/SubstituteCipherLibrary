fe3a15aeeef5a6cbc9d9beca9fa40d64c38ee58a
soesec13 <sebi.wurst@gmail.com>
2016-08-18 17:37:00 +0200

    Added Access to WordList

---
+++ SubstituteCipherLibrary/src/com/soesec/util/WordListAccess.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soesec.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author sebi
+ */
+public class WordListAccess {
+    private String[] words;
+    private final String path = "src\\com\\soesec\\data\\words.txt";
+    public static final String seperators = "[\n:,. !]";
+    private WordListAccess() {
+        try 
+        {
+            File f = new File(path);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(f));
+            List<String> lines = new ArrayList<String>();
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+            bufferedReader.close();
+            words = lines.toArray(new String[lines.size()]);
+        } 
+        catch (Exception e) 
+        {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+    
+    public boolean isAWord(String word)
+    {
+        word = word.toUpperCase();
+        int index=Arrays.binarySearch(words,word);
+        return index >= 0;
+    }
+    
+    public boolean containsAWord(String sentence)
+    {
+        String[] words = sentence.split(seperators);
+        for(String word : words)
+        {
+           if(isAWord(word))
+           {
+               return true;
+           }
+        }
+        return false;
+    }
+    public double percentageOfWords(String sentence)
+    {
+        String[] words = sentence.split(seperators);
+        int emptyWords= 0;
+        int wordCount=0;
+        for(String word : words)
+        {
+            if(word.isEmpty())
+            {
+                emptyWords++;
+                continue;
+            }
+            if(isAWord(word))
+            {
+                wordCount++;
+            }
+        }
+        return (wordCount / (double) (words.length - emptyWords)) * 100.0;
+    }
+    public boolean isASentence(String sentence)
+    {
+        return percentageOfWords(sentence) == 100;
+    }
+    
+    public static WordListAccess getInstance() {
+        return WordListAccessHolder.INSTANCE;
+    }
+    
+    private static class WordListAccessHolder {
+
+        private static final WordListAccess INSTANCE = new WordListAccess();
+    }
+}
---
+++ SubstituteCipherLibrary/test/demo/WordAccessDemo.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package demo;
+
+import com.soesec.util.WordListAccess;
+
+
+public class WordAccessDemo {
+
+    public static void main(String[] args) {
+        WordListAccess access = WordListAccess.getInstance();
+        String word = "hello";
+        System.out.println("Word: "+word);
+        if(access.isAWord(word))
+        {
+            System.out.println("Is a word");
+        }
+        else
+        {
+            System.out.println("Not a word");
+        }
+        String notWord = "asdfadsfasdf";
+        System.out.println("Word: "+notWord);
+        if(access.isAWord(notWord))
+        {
+            System.out.println("Is a word");
+        }
+        else
+        {
+            System.out.println("Not a word");
+        }
+        String sentence = "Hello dear World! I love you";
+        System.out.println("Sentence: "+sentence);
+        if(access.isASentence(sentence))
+        {
+            System.out.println("Is a sentence");
+        }
+        else
+        {
+            System.out.println("Is not a sentence");
+        }
+        
+        String partlySentence = "Hello dear World! I aslödkf you";
+        System.out.format("Sentence: %s\n is to %.2f%s a sentence", partlySentence,access.percentageOfWords(partlySentence),'%');
+        
+        
+    }
+
+}
